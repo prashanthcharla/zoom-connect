c922de62a70d4c4d52775db1979db16dd230dc5d
Prashanth Charla <prashanthcharla1234@gmail.com>
2023-07-01 12:29:42 +0530

    Install firebase & zoom related libraries
    + Create environment.ts file

---
+++ zoom-connect-ui/src/app/Constants.ts
@@ -0,0 +1,9 @@
+export const FIREBASE_CONFIG = {
+    apiKey: "AIzaSyBVlFWT5ibHQFdCKH7ZtGuQF5Fdb1dwtBM",
+    authDomain: "fir-ebd55.firebaseapp.com",
+    projectId: "fir-ebd55",
+    storageBucket: "fir-ebd55.appspot.com",
+    messagingSenderId: "653107276940",
+    appId: "1:653107276940:web:60aaee27d19cc900255fa9",
+    measurementId: "G-3LGMC4EJZJ"
+};
--- zoom-connect-ui/src/app/app.module.ts
+++ zoom-connect-ui/src/app/app.module.ts
@@ -1,12 +1,30 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgMultiSelectDropDownModule } from 'ng-multiselect-dropdown';
+import { ReactiveFormsModule } from '@angular/forms';
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFireDatabaseModule } from '@angular/fire//compat/database';
+import { AngularFireAuthModule } from '@angular/fire/compat/auth';
+import { AngularFireMessagingModule } from '@angular/fire/compat/messaging';
+import { FIREBASE_CONFIG } from './Constants';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    AngularFireModule.initializeApp(FIREBASE_CONFIG),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule,
+    AngularFireMessagingModule,
+    NgMultiSelectDropDownModule
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
