c43593a8cfb0f847cedd1d1afa546b1b1c64a1ee
Prashanth Charla <prashanthcharla1234@gmail.com>
2023-07-01 08:53:28 +0530

    Create project skeleton

---
+++ src/main/java/com/prashanth/zoomconnect/ZoomConnectApplication.java
@@ -0,0 +1,13 @@
+package com.prashanth.zoomconnect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZoomConnectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZoomConnectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/prashanth/zoomconnect/ZoomConnectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prashanth.zoomconnect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZoomConnectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
