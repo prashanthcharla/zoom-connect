a597a66ff001a31765bc947c0600f0c88571bc2f
Prashanth Charla <prashanthcharla1234@gmail.com>
2023-07-12 12:08:33 +0530

    Remove "GET Signature" API
    + Instead, we can use inbuilt ZoomMtg method on UI

--- src/main/java/com/prashanth/zoomconnect/model/AuthResponse.java
+++ src/main/java/com/prashanth/zoomconnect/model/AuthResponse.java
@@ -7,4 +7,5 @@ public class AuthResponse {
 	String encryptedUserId;
 	String encryptedUserName;
 	String encryptedApiKey;
+	String encryptedApiSecret;
 }
--- src/main/java/com/prashanth/zoomconnect/service/AuthServiceImpl.java
+++ src/main/java/com/prashanth/zoomconnect/service/AuthServiceImpl.java
@@ -1,11 +1,6 @@
 package com.prashanth.zoomconnect.service;
 
-import java.security.InvalidKeyException;
-import java.security.NoSuchAlgorithmException;
 import java.util.Base64;
-
-import javax.crypto.Mac;
-import javax.crypto.spec.SecretKeySpec;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -19,31 +14,14 @@ public class AuthServiceImpl implements AuthService {
 	@Autowired
 	private ZoomMetadata zoomMetadata;
 
-	private static final String SIGNATURE_ENCR_ALGORITHM = "HmacSHA256";
-
-	@Override
-	public String generateSignature(String apiKey, String apiSecret, String meetingNumber, long timestamp, int role)
-			throws NoSuchAlgorithmException, InvalidKeyException {
-		String message = apiKey + meetingNumber + timestamp + role;
-		byte[] hmacBytes = this.calculateHmac(message.getBytes(), apiSecret.getBytes());
-		String hash = Base64.getEncoder().encodeToString(hmacBytes);
-		return apiKey + "." + meetingNumber + "." + timestamp + "." + role + "." + hash;
-	}
-
 	@Override
 	public AuthResponse getApiKey() {
 		AuthResponse apiKeyResponse = new AuthResponse();
 		apiKeyResponse.setEncryptedUserId(getBase64EncodedString(zoomMetadata.getUserId()));
 		apiKeyResponse.setEncryptedUserName(getBase64EncodedString(zoomMetadata.getUserName()));
 		apiKeyResponse.setEncryptedApiKey(getBase64EncodedString(zoomMetadata.getClientId()));
+		apiKeyResponse.setEncryptedApiSecret(getBase64EncodedString(zoomMetadata.getClientSecret()));
 		return apiKeyResponse;
-	}
-
-	private byte[] calculateHmac(byte[] data, byte[] key) throws NoSuchAlgorithmException, InvalidKeyException {
-		SecretKeySpec secretKey = new SecretKeySpec(key, SIGNATURE_ENCR_ALGORITHM);
-		Mac mac = Mac.getInstance(SIGNATURE_ENCR_ALGORITHM);
-		mac.init(secretKey);
-		return mac.doFinal(data);
 	}
 
 	private String getBase64EncodedString(String text) {
