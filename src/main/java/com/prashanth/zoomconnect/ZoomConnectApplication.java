5809088c93003dc55e4cc52da5849f364b57d2df
Prashanth Charla <prashanthcharla1234@gmail.com>
2023-07-12 12:06:09 +0530

    Enable scheduling to refresh access token

--- src/main/java/com/prashanth/zoomconnect/ZoomConnectApplication.java
+++ src/main/java/com/prashanth/zoomconnect/ZoomConnectApplication.java
@@ -3,11 +3,13 @@ package com.prashanth.zoomconnect;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 import com.prashanth.zoomconnect.config.SwaggerConfigProps;
 import com.prashanth.zoomconnect.config.ZoomMetadata;
 
 @SpringBootApplication
+@EnableScheduling
 @EnableConfigurationProperties({ SwaggerConfigProps.class, ZoomMetadata.class })
 public class ZoomConnectApplication {
 
