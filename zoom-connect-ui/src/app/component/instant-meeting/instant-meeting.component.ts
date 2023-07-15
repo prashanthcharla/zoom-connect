0df42b1f1da18e4ebe12d95e2a7beff7f3fb169e
Prashanth Charla <prashanthcharla1234@gmail.com>
2023-07-15 11:21:25 +0530

    Replace participant component with reusable component

--- zoom-connect-ui/src/app/component/instant-meeting/instant-meeting.component.ts
+++ zoom-connect-ui/src/app/component/instant-meeting/instant-meeting.component.ts
@@ -1,8 +1,5 @@
-import { LiveAnnouncer } from '@angular/cdk/a11y';
-import { COMMA, ENTER } from '@angular/cdk/keycodes';
-import { Component, OnInit, inject } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
-import { MatChipEditedEvent, MatChipInputEvent } from '@angular/material/chips';
 import { Subscription } from 'rxjs';
 import { MeetingRequest } from 'src/app/model/request/Meeting.request.data';
 import { MeetingService } from 'src/app/service/meeting.service';
@@ -14,10 +11,8 @@ import { MeetingService } from 'src/app/service/meeting.service';
 })
 export class InstantMeetingComponent implements OnInit {
 
-  separatorKeysCodes: number[];
   selectedParticipants: Array<string>;
   topicControl: FormControl;
-  announcer: LiveAnnouncer;
   enableControls: boolean;
   enableSpinner: boolean;
 
@@ -26,10 +21,8 @@ export class InstantMeetingComponent implements OnInit {
   constructor(
     private _meetingService: MeetingService
   ) {
-    this.separatorKeysCodes = [ENTER, COMMA];
     this.selectedParticipants = new Array<string>();
     this.topicControl = new FormControl();
-    this.announcer = inject(LiveAnnouncer);
     this.enableControls = true;
     this.enableSpinner = false;
     this._subscriptions = new Subscription();
@@ -51,38 +44,8 @@ export class InstantMeetingComponent implements OnInit {
     );
   }
 
-  addParticipant(event: MatChipInputEvent): void {
-    const value = (event.value || '').trim();
-    if (value) {
-      this.selectedParticipants.push(value);
-    }
-    event.chipInput!.clear();
-  }
-
-  editParticipant(participant: string, event: MatChipEditedEvent) {
-    const value = event.value.trim();
-
-    // Remove participent if it no longer has a value
-    if (!value) {
-      this.removeParticipant(participant);
-      return;
-    }
-
-    // Edit existing participent
-    const index = this.selectedParticipants.indexOf(participant);
-    if (index >= 0) {
-      this.selectedParticipants[index] = value;
-    }
-  }
-
-  removeParticipant(participant: string): void {
-    const index = this.selectedParticipants.indexOf(participant);
-
-    if (index >= 0) {
-      this.selectedParticipants.splice(index, 1);
-
-      this.announcer.announce(`Removed ${participant}`);
-    }
+  updateParticipants(values: Array<string>) {
+    this.selectedParticipants = values;
   }
 
   startMeeting(): void {
@@ -97,5 +60,4 @@ export class InstantMeetingComponent implements OnInit {
       })
     );
   }
-
 }
